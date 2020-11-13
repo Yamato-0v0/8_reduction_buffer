03d63f83ca1eed55e2c9f743c2c9ea9783dcb524
Yamato-0v0 <g1827022@st.t-kougei.ac.jp>
2020-11-14 00:06:37 +0900

    LectureContent

---
+++ src/Assets/Scenes/ggNewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ggNewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //float distance = Camera.main.faClipPlane - 3.0f;
+        float distance = transform.localPosition.z;
+        transform.localPosition = new Vector3(0, 0, distance);
+        float inv_aspect = (float)Screen.width / (float)Screen.height;
+        float s = Mathf.Tan(0.5f * Camera.main.fieldOfView * Mathf.Deg2Rad) * 2.0f * distance;
+        transform.localScale = new Vector3(inv_aspect * s, s, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
